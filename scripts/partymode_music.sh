a3c82059f509fa42ebbaaa691e119cd905d62b38
Michel Vrauwdeunt <zanbee@bragi.fritz.box>
2021-01-09 11:13:50 +0100

    Basic Setup ready

---
+++ scripts/partymode_music.sh
@@ -0,0 +1,1 @@
+curl --user Kodi:"\qYCju=xQPsD3qqs" --header 'Content-Type: application/json' --data-binary '{ "jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "partymode": "music" } }, "id": 1 }' 'http://192.168.178.200:80/jsonrpc'
---
+++ scripts/stopsleep.sh
@@ -0,0 +1,2 @@
+curl --user Kodi:"\qYCju=xQPsD3qqs" --header 'Content-Type: application/json' --data-binary '{"jsonrpc": "2.0", "method": "Player.Stop", "params": { "playerid": 0 }, "id": 1}' 'http://192.168.178.200:80/jsonrpc'
+

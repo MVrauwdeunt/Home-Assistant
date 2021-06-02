a3c3cd14717169935648d453fe8f691bc4ad23c6
root <root@bragi.fritz.box>
2021-06-02 22:22:05 +0200

    config files on 02-06-2021 22:22:05

--- scripts/shopping_list.py
+++ scripts/shopping_list.py
@@ -6,11 +6,11 @@ import json
 with open('/config/.shopping_list.json') as data_file:
     shoppingListData = json.load(data_file)
 
-content = u"Boodschappen:\n"
-for entry in shoppingListData:
-    if not entry['complete']:
-        content += u"- %s\n" % entry['name']
+# content = u"Boodschappen:\n"
+# for entry in shoppingListData:
+#     if not entry['complete']:
+#         content += u"- %s\n" % entry['name']
 
-content += u"\n"
-
+# content += u"\n"
+content = u"['boodschappen':{'kaas','ham'}]"
 print(content)

6e7a2709ac6ded123041b0d75c31280b896a3661
root <root@bragi.fritz.box>
2021-07-31 19:57:42 +0200

    config files on 31-07-2021 19:57:42

--- scripts/kodi_audio_21.sh
+++ scripts/kodi_audio_21.sh
@@ -3,4 +3,4 @@ user="$(grep ^kodi_user: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 pass="$(grep ^kodi_pass: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 ip="$(grep ^baldr_ip_address: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 
-curl --user $user:$pass -H "Content-type: application/json" -X POST -d '{"jsonrpc":"2.0","method":"Settings.SetSettingValue", "params":{"setting":"audiooutput.audiodevice","value":"surround21:CARD=Device,DEV=0"},"id":1}' http://$ip:80/jsonrpc
+curl --user $user:$pass -H "Content-type: application/json" -d '{ "jsonrpc": "2.0", "method": "Settings.SetSettingValue", "params":{ "setting": "audiooutput.audiodevice", "value": "surround21:CARD=Device,DEV=0"}, "id": 1}' http://$ip:80/jsonrpc
--- scripts/partymode_music.sh
+++ scripts/partymode_music.sh
@@ -3,4 +3,4 @@ user="$(grep ^kodi_user: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 pass="$(grep ^kodi_pass: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 ip="$(grep ^baldr_ip_address: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 
-curl --user $user:$pass --header 'Content-Type: application/json' --data-binary '{ "jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "partymode": "music" } }, "id": 1 }' 'http://'$ip':80/jsonrpc'
+curl --user $user:$pass -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "partymode": "music" } }, "id": 1 }' 'http://'$ip':80/jsonrpc'

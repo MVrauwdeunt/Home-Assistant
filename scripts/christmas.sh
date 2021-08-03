85af533b6b9c937a5aef5177c49c852e58765ebe
root <root@bragi.fritz.box>
2021-08-03 10:18:13 +0200

    config files on 03-08-2021 10:18:13

--- scripts/christmas.sh
+++ scripts/christmas.sh
@@ -3,4 +3,4 @@ user="$(grep ^kodi_user: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 pass="$(grep ^kodi_pass: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 ip="$(grep ^baldr_ip_address: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
 
-curl --user $user:$pass -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "partymode": "music" } }, "id": 1 }' 'http://'$ip':80/jsonrpc'
+curl --user $user:$pass -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":"1","method":"Player.Open","params":{"item":{"file":"special://profile/playlists/music/christmas.xsp"}}};'http://'$ip':80/jsonrpc'

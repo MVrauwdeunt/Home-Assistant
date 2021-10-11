7bb0dd22e297da4ddbe24cf2080d61ff3af842e3
root <root@bragi.fritz.box>
2021-10-11 10:23:40 +0200

    config files on 11-10-2021 10:23:40

--- scripts/kodi_audio_51.sh
+++
@@ -1,6 +0,0 @@
-secrets="../secrets.yaml"
-user="$(grep ^kodi_user: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
-pass="$(grep ^kodi_pass: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
-ip="$(grep ^baldr_ip_address: $secrets | sed 's/^[^:]*: //' | sed 's/ //g')"
-
-curl --user $user:$pass -H "Content-type: application/json" -d '{ "jsonrpc": "2.0", "method": "Settings.SetSettingValue", "params": {"setting": "audiooutput.audiodevice", "value": "surround51:CARD=Device,DEV=0"}, "id": 1}' http://$ip:80/jsonrpc

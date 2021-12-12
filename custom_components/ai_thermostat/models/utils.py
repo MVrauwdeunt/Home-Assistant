8e9d80880d38a3e9f63f2bdc7d41ee1def19f4ce
root <root@bragi.fritz.box>
2021-12-12 14:24:23 +0100

    config files on 12-12-2021 14:24:23

--- custom_components/ai_thermostat/models/utils.py
+++ custom_components/ai_thermostat/models/utils.py
@@ -1,5 +1,6 @@
 import asyncio
 import math
+from custom_components.ai_thermostat.helpers import convert_decimal
 from homeassistant.helpers.json import JSONEncoder
 import logging
 from datetime import datetime, timedelta
@@ -17,15 +18,22 @@ class cleanState:
 
 def default_calibration(self):
   state = self.hass.states.get(self.heater_entity_id).attributes
-  #new_calibration = float(round((float(self._cur_temp) - float(state.get('local_temperature'))) + float(state.get('local_temperature_calibration')),2))
-  new_calibration = int(math.ceil((math.floor(float(self._cur_temp)) - round(float(state.get('local_temperature')))) + round(float(state.get('local_temperature_calibration')))))
+  #new_calibration = int(math.ceil((math.floor(float(self._cur_temp)) - round(float(state.get('local_temperature')))) + round(float(state.get('local_temperature_calibration')))))
+  # temp range fix
+  new_calibration = float((float(self._cur_temp) - float(state.get('local_temperature'))) + float(state.get('local_temperature_calibration')))
+  if new_calibration > 0 and new_calibration < 1:
+    new_calibration = round(new_calibration)
+  if new_calibration < -6:
+      new_calibration = -6
+  if new_calibration > 6:
+      new_calibration = 6
 
-  return new_calibration
+  return convert_decimal(new_calibration)
 
 async def overswing(self,calibration):
   state = self.hass.states.get(self.heater_entity_id).attributes
   mqtt = self.hass.components.mqtt
-  if (datetime.now() > (self.lastOverswing + timedelta(minutes = 5))) and state.get('system_mode') is not None and self._target_temp is not None and self._cur_temp is not None and not self.night_status:
+  if (datetime.now() > (self.lastOverswing + timedelta(minutes = 15))) and state.get('system_mode') is not None and self._target_temp is not None and self._cur_temp is not None and not self.night_status:
     check_overswing = (float(self._target_temp) - 0.5) < float(self._cur_temp)
     if check_overswing:
       self.ignoreStates = True
